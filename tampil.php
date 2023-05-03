5fcde9f9f00b02c6b2124905f63ba071db02bf75
KrisnaDiva <krisnadiva0412@gmail.com>
2023-05-03 21:35:30 +0700

    Pertemuan 6

--- tampil.php
+++ tampil.php
@@ -2,6 +2,7 @@
 include("koneksi.php");
 $sql="select * from mahasiswa";
 $proses=mysqli_query($koneksi,$sql);
+$isi = mysqli_fetch_assoc($proses);
     echo "NIM                       :".$isi['nim']."<br>";
     echo "Nama                      :".$isi['nama']."<br>";
     echo "Jenis Kelamin             :".$isi['jenkel']."<br>";
